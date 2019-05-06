5b4cb2e020d8cdda78a3d529b31c6c0844c03a25
JEFF ENG <ME@JEFFENG.COM>
2019-05-06 14:35:11 -0700

    Updated admin.py to register the models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from inventory.models import Category, Asset, AssetInstance
+
+admin.site.register(Category)
+admin.site.register(Asset)
+admin.site.register(AssetInstance)
