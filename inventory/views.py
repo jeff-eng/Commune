4a809920fb5e51ad6162830783b77a009f0f9eba
JEFF ENG <ME@JEFFENG.COM>
2019-05-11 13:21:25 -0700

    Added a urls.py file for inventory app. Also defined the view function for the index page.

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from inventory.models import Category, Asset, AssetInstance
 
-# Create your views here.
+# View function for "home" page - processed the HTTP request, fetches required data from database, and renders data in HTML page using template, and returns generated HTML in HTTP Response
+def index(request):
+    """View function for the home page of the site """
+
+    # Generate counts of some of the main objects
+    num_assets = Asset.objects.all().count()
+    num_instances = AssetInstance.objects.all().count()
+
+    # Checked out items
+    num_instances_checkedout = AssetInstance.objects.filter(checked_out=True).count()
+
+    context = {
+        'num_assets': num_assets,
+        'num_instances': num_instances,
+        'num_instances_checkedout': num_instances_checkedout,
+    }
+
+    return render(request, 'index.html', context=context)
