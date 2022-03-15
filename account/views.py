791380cee3cdf063f342e7ca6db9478114dfb78f
JEFF ENG <ME@JEFFENG.COM>
2022-03-15 16:06:09 -0700

    Implemented password change functionality.

--- account/views.py
+++ account/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from account.forms import UserRegistrationForm
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.forms import PasswordChangeForm
+from account.forms import ChangePasswordForm, UserRegistrationForm
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.urls import reverse_lazy
 
 class UserRegistrationView(View):
     form_class = UserRegistrationForm
@@ -22,6 +25,13 @@ class UserRegistrationView(View):
             raw_password = form.cleaned_data.get('password1')
             account = authenticate(email=email, password=raw_password)
             login(request, account)
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect('/dashboard')
 
         return render(request, self.template_name, {'registration_form': form})
+
+class ChangePasswordView(PasswordChangeView):
+    form_class = ChangePasswordForm
+    success_url = reverse_lazy('password_success')
+
+def password_success(request):
+    return render(request, 'account/password_success.html', {})
